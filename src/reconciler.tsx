4041fa6435f1ca23f60db1e5c6e62d4710674c28
Nicolas Forestier <forestier.mk@gmail.com>
2019-12-06 15:30:27 +0100

    Update reconciler.tsx

--- src/reconciler.tsx
+++ src/reconciler.tsx
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import Reconciler from 'react-reconciler'
-import addInstancedMesh from 'three-instanced-mesh' )
+import addInstancedMesh from 'three-instanced-mesh'
 import { unstable_now as now, unstable_IdlePriority as idlePriority, unstable_runWithPriority as run } from 'scheduler'
 import { CanvasContext } from './canvas'
 import { version } from '../package.json'
